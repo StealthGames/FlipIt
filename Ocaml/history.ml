5fd128aebe891907f00ede8bceb075a66f28e0a3
Baptiste Lefebvre <baptiste.lefebvre@ens.fr>
2013-04-15 00:32:40 +0200

    Add a LaTeX generator

--- Ocaml/history.ml
+++ Ocaml/history.ml
@@ -10,22 +10,22 @@ type history =
 
 (* Reference ******************************************************************)
 
-let history = ref
+let history =
 {
-	player1 = [0.];
-	player2 = [  ];
+	player1 = [];
+	player2 = [];
 	last_flip = 0.;
 }
 
 
 (* Functions ******************************************************************)
 
-let init_history () =
-	history.player1 <- [0.];
-	history.player2 <- [  ];
+let init () =
+	history.player1 <- [];
+	history.player2 <- [];
 	history.last_flip <- 0.
 
-let add_to_history player_number time =
+let add player_number time =
 	if (time < history.last_flip)
 	then failwith "History.add_to_history Error: Invalid flip time";
 	history.last_flip <- time;
@@ -34,6 +34,6 @@ let add_to_history player_number time =
 		| 2 -> history.player2 <- time :: history.player2
 		| _ -> failwith "History.add_to_history Error: Unknow player number"
 
-let get_history () =
-	(history.player1, history.player2)
+let print () =
+	Printer.latex history.player1 history.player2
 

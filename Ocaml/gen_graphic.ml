696a982c62c14bc60a95313b70828b616686734d
simon <simon@albatros.(none)>
2013-04-19 15:52:05 +0200

    Réorganisation du dossier Latex. Modifications de la génération graphique.

--- Ocaml/gen_graphic.ml
+++ Ocaml/gen_graphic.ml
@@ -1,14 +1,18 @@
 let gen_graphic tab1 tab2 =
-  let tab1 = List.sort Pervasives.compare tab1 and tab2 = List.sort Pervasives.compare tab2 in
+  let maxpos = max (List.fold_left max 0. tab1) (List.fold_left max 0. tab2) in 
+  let tab1 = List.map (function x -> x/.maxpos*.12.) (List.sort Pervasives.compare tab1) and tab2 = List.map (function x -> x/.maxpos*.12.) (List.sort Pervasives.compare tab2) in
   let rec aux lastpos lastplayer = function
-      ([],[]) -> Format.sprintf "\\drawrect{%f}{%f}{%f}{%f}{%s}\n" lastpos 13. 0. 1. (if lastplayer = 1 then "blue" else "red") 
-    | (a::q, []) -> (if lastplayer <> 1 then Format.sprintf "\\drawrect{%f}{%f}{%f}{%f}{%s}\n" lastpos a (-1.) 0. "red" else "")^(aux a 1 (q,[])) 
-    | ([], a::q) -> (if lastplayer <> -1 then Format.sprintf "\\drawrect{%f}{%f}{%f}{%f}{%s}\n" lastpos a 0. 1. "blue" else "")^(aux a (-1) ([],q))
-    | (a1::q1,a2::q2) when a1 <= a2 -> (if lastplayer <> 1 then Format.sprintf "\\drawrect{%f}{%f}{%f}{%f}{%s}\n" lastpos a1 (-1.) 0. "red" else "")^(aux (if lastplayer <> 1 then a1 else lastpos) 1 (q1,a2::q2))
-    | (a1::q1,a2::q2) -> (if lastplayer <> -1 then Format.sprintf "\\drawrect{%f}{%f}{%f}{%f}{%s}\n" lastpos a2 0. 1. "blue" else "")^(aux (if lastplayer <> -1 then a2 else lastpos) (-1) (a1::q1,q2)) in
-  let rec aux2 height color = function
+      ([],[]) -> Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos 13. lastplayer 
+    | (a::q, []) -> (
+        if lastplayer <> 1 
+           then Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos a (-1)
+           else "")^(aux a 1 (q,[])) 
+    | ([], a::q) -> (if lastplayer <> -1 then Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos a 1 else "")^(aux a (-1) ([],q))
+    | (a1::q1,a2::q2) when a1 <= a2 -> (if lastplayer <> 1 then Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos a1 (-1) else "")^(aux (if lastplayer <> 1 then a1 else lastpos) 1 (q1,a2::q2))
+    | (a1::q1,a2::q2) -> (if lastplayer <> -1 then Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos a2 1 else "")^(aux (if lastplayer <> -1 then a2 else lastpos) (-1) (a1::q1,q2)) in
+  let rec aux2 player = function
       [] -> ""
-    | a::q -> (Format.sprintf "\\drawbullet{%f}{%f}{%s}\n" a height color)^(aux2 height color q) in
-    (aux2 2. "blue" tab1)^(aux2 (-2.) "red" tab2)^(aux 0. 1 (tab1,tab2))
+    | a::q -> (Format.sprintf "\\drawbulletforplayer{%f}{%d}\n" a player)^(aux2 player q) in
+    (aux2 1 tab1)^(aux2 (-1) tab2)^(aux 0. 1 (tab1,tab2))
 let _ = print_string (gen_graphic [1.; 3.; 5.; 7.;9.] [0.5; 5.; 6.1;7.5])
 

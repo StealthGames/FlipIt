edecbfb99c99d7919a84534d8de4856106a34969
simon <simon@albatros.(none)>
2013-04-19 17:47:44 +0200

    My commit is cool :D tu connais pas twitter??!!!

--- Ocaml/gen_graphic.ml
+++ Ocaml/gen_graphic.ml
@@ -1,8 +1,8 @@
 let gen_graphic tab1 tab2 =
   let maxpos = max (List.fold_left max 0. tab1) (List.fold_left max 0. tab2) in 
-  let tab1 = List.map (function x -> x/.maxpos*.12.) (List.sort Pervasives.compare tab1) and tab2 = List.map (function x -> x/.maxpos*.12.) (List.sort Pervasives.compare tab2) in
+  let tab1 = List.map (function x -> x/.maxpos*.16.) (List.sort Pervasives.compare tab1) and tab2 = List.map (function x -> x/.maxpos*.16.) (List.sort Pervasives.compare tab2) in
   let rec aux lastpos lastplayer = function
-      ([],[]) -> Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos 13. lastplayer 
+      ([],[]) -> Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos 17. lastplayer 
     | (a::q, []) -> (
         if lastplayer <> 1 
            then Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos a (-1)
@@ -13,6 +13,12 @@ let gen_graphic tab1 tab2 =
   let rec aux2 player = function
       [] -> ""
     | a::q -> (Format.sprintf "\\drawbulletforplayer{%f}{%d}\n" a player)^(aux2 player q) in
-    (aux2 1 tab1)^(aux2 (-1) tab2)^(aux 0. 1 (tab1,tab2))
-let _ = print_string (gen_graphic [1.; 3.; 5.; 7.;9.] [0.5; 5.; 6.1;7.5])
+    (aux2 1 tab1)^(aux2 (-1) tab2)^(aux 0. (if List.hd tab1 <= List.hd tab2 then 1 else -1) (tab1,tab2))
+let calc_listn n =
+  let rec aux = function
+   0 -> [0]
+     | n -> n::(aux (n-1))
+  in List.rev (aux n) 
+
+let _ = print_string (gen_graphic (List.map (function x -> 7.*.(float_of_int x) +. 0.5) (calc_listn 4))  (List.map (function x -> 5.*.(float_of_int x)) (calc_listn 5)))
 

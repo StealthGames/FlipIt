43bd05f7330afe760ac874809982a4ae9464c8c0
simon <simon@albatros.(none)>
2013-05-17 01:58:31 +0200

    This commit message is a lie.

--- Ocaml/gen_graphic.ml
+++ Ocaml/gen_graphic.ml
@@ -5,8 +5,8 @@ let gen_graphic tab1 tab2 =
       ([],[]) -> Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos 17. lastplayer 
     | (a::q, []) -> (
         if lastplayer <> 1 
-           then Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos a (-1)
-           else "")^(aux a 1 (q,[])) 
+        then Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos a (-1)
+        else "")^(aux a 1 (q,[])) 
     | ([], a::q) -> (if lastplayer <> -1 then Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos a 1 else "")^(aux a (-1) ([],q))
     | (a1::q1,a2::q2) when a1 <= a2 -> (if lastplayer <> 1 then Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos a1 (-1) else "")^(aux (if lastplayer <> 1 then a1 else lastpos) 1 (q1,a2::q2))
     | (a1::q1,a2::q2) -> (if lastplayer <> -1 then Format.sprintf "\\drawrectforplayer{%f}{%f}{%d}\n" lastpos a2 1 else "")^(aux (if lastplayer <> -1 then a2 else lastpos) (-1) (a1::q1,q2)) in
@@ -16,9 +16,12 @@ let gen_graphic tab1 tab2 =
     (aux2 1 tab1)^(aux2 (-1) tab2)^(aux 0. (if List.hd tab1 <= List.hd tab2 then 1 else -1) (tab1,tab2))
 let calc_listn n =
   let rec aux = function
-   0 -> [0]
-     | n -> n::(aux (n-1))
+      0 -> [0]
+    | n -> n::(aux (n-1))
   in List.rev (aux n) 
 
-let _ = print_string (gen_graphic (List.map (function x -> 7.*.(float_of_int x) +. 0.5) (calc_listn 4))  (List.map (function x -> 5.*.(float_of_int x)) (calc_listn 5)))
+(*let _ = print_string (gen_graphic (List.map (function x -> 7.*.(float_of_int
+ * x) +. 5.5) (calc_listn 3))  (List.map (function x -> 4.*.(float_of_int x))
+ * (calc_listn 4)))*)
+let _ = print_string (gen_graphic [0.; 1.; 1.7] [0.2;1.2;2.2]) 
 
